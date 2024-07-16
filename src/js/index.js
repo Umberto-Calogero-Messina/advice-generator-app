3306667054cce3f93b54e379b50ac4275058833b
Genezis <ucmessina@alumnostrazos.net>
2024-07-16 12:01:52 +0200

    +Update Work

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const generatorElement = document.getElementById('button-generator');
+const idElement = document.getElementById('span-id');
+const textElement = document.getElementById('span-text');
+
+const generateTextFunction = async url => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    idElement.textContent = data.slip.id;
+    textElement.textContent = `"${data.slip.advice}"`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+generateTextFunction('https://api.adviceslip.com/advice');
+
+generatorElement.addEventListener('click', () => generateTextFunction('https://api.adviceslip.com/advice'));
